fb5a00cc4aeb4194934ec941c06e79b33a8e72d5
Juergen Hoetzel <juergen@hoetzel.info>
2021-03-05 21:23:43 +0100

    Initial MMX version

---
+++ asm_util.c
@@ -0,0 +1,28 @@
+/* asm helper functions */
+#include "sepia.h"
+
+void convert_to_b4g4r4(pixel *src, uint32_t dst[12]) {
+  /* b */
+  dst[0] = src[0].b;
+  dst[1] = src[0].b;
+  dst[2] = src[0].b;
+  dst[3] = src[1].b;
+  /* g */
+  dst[4] = src[0].g;
+  dst[5] = src[0].g;
+  dst[6] = src[0].g;
+  dst[7] = src[1].g;
+  /* r */
+  dst[8] = src[0].r;
+  dst[9] = src[0].r;
+  dst[10] = src[0].r;
+  dst[11] = src[1].r;
+}
+
+void convert_from_b4g4r4(uint32_t src[12], pixel *dst) {
+  /* b */
+  dst[0].b = src[0];
+  dst[0].g = src[1];
+  dst[0].r = src[2];
+  dst[3].b = src[3];
+}
--- sepia.asm
+++ sepia.asm
@@ -1,6 +1,48 @@
 global sepia_one
 
+extern convert_to_b4g4r4
+extern convert_from_b4g4r4
+
+section .data
+
+col1: dd 0.393,0.349,0.272,0.393
+col2: dd 0.769,0.686,0.168,0.768
+col3: dd 0.189,0.168,0.131,0.189
+
 section .text
+
 sepia_one:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 512		;FIXME How many
+	mov rsi, rbp
+	sub rsi, 512
+	call convert_to_b4g4r4
+	CVTDQ2PS xmm0, [rsi]
+	CVTDQ2PS xmm1, [rsi-16]
+	CVTDQ2PS xmm2, [rsi-32]
+
+	MOVAPS xmm3, [rel col1]
+	MOVAPS xmm4, [rel col2]
+	MOVAPS xmm5, [rel col3]
+
+	mulps xmm0, xmm3
+	mulps xmm1, xmm4
+	mulps xmm2, xmm5
+
+	;; FIXME can use ::  DPPS — Dot Product of Packed Single Precision Floating-Point Values
+	addps xmm0, xmm1
+	addps xmm0, xmm2
+	addps xmm0, xmm3
+	;; xmm0 = b1|g1|r1|b2
+	;; FIXME saturatio
+	CVTPS2DQ xmm0, xmm0
+	movdqa [rsi], xmm0
+	xchg rsi, rdi
+	call convert_from_b4g4r4
+	;; FIXME: ignore 3/4 pixels
+	mov rax, 0x4
+	mov rsp, rbp
+	pop rbp
 	ret
 
